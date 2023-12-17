e2f28789510e33bdd2a5901b5188d7248104a452
askhan963 <awaisknas963@gmail.com>
2023-12-17 14:46:45 +0500

    Session 8

---
+++ session-8/script.js
@@ -0,0 +1,45 @@
+
+
+
+// const head = document.getElementById('head')
+
+
+// console.log(head)
+
+// // head.innerHTML= "Session 8"
+
+// const btn = document.getElementById('btn')
+// console.log(btn)
+
+// btn.addEventListener('click',()=>{
+//     head.innerHTML= "Session 8"
+// })
+
+
+// console.log(document)
+
+// Query Selectors
+// const x = document.querySelector("h2")
+// console.log(x)
+// x.innerHTML = "This Element has been Selected."
+
+
+// const y = document.querySelectorAll('h2')
+// console.log(y)
+
+// y.forEach((el)=>{
+//     console.log(el)
+// el.innerHTML= "Comsats..."
+// })
+
+// class Selectors
+
+// const x1 = document.getElementsByClassName('h2')
+// console.log(x1)
+
+// for (let i=0 ; i<x1.length ;i++){
+// console.log(x1[i])
+// x1[i].innerHTML= "Abc"
+// }
+
+
